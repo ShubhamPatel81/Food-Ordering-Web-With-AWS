a7c46b066add22957376d11e596d85b83884c5d8
Shubham Patel <shubhampatel872005@gmail.com>
2025-07-19 22:05:32 +0530

    adding the client side frontend

--- Food-Web-Client-Panel/src/assets/assets.js
+++ Food-Web-Client-Panel/src/assets/assets.js
@@ -1,9 +1,22 @@
 import logo from './logo.png';
 import cart from './images/cart.png';
+import pizza from './pizza1.jpg';
+import burger from './images/burger2.jpg';
+import burger1 from './images/burger3.jpg';
+import headPhone from './images/headphone_1.jpeg';
+import headPhone2 from './images/headphone_2.jpeg';
+import mouse from './images/mouse_1.jpeg';
+
 const assets = {
   logo,
   cart,
-
+  pizza,
+  burger,
+  burger1,
+  headPhone,
+  headPhone2,
+  mouse,
 };
 
 export default assets;
+
